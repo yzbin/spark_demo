73f7a97c91c6e4361bde203aedd2a3603162902c
robin <jiabin126email@126.com>
2017-12-22 11:13:55 +0800

    ParquetMergeSchema commit

--- src/main/scala/com/robin/sparksql/ParquetMergeSchema.scala
+++ src/main/scala/com/robin/sparksql/ParquetMergeSchema.scala
@@ -12,7 +12,7 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object ParquetMergeSchema {
   def main(args: Array[String]): Unit = {
-    System.setProperty("hadoop.home.dir", "C:\\hadoop-2.7.0")
+    System.setProperty("hadoop.home.dir","C:\\hadoop-2.7.0")
     val conf = new SparkConf()
       .setAppName("ParquetMergeSchema")
       .setMaster("local[*]")
@@ -20,22 +20,34 @@ object ParquetMergeSchema {
     val sqlContext = new SQLContext(sc)
 
     import sqlContext.implicits._
+
     // 创建一个DataFrame，作为学生的基本信息，并写入一个parquet文件中
-    val studentWithNameAge = Array(
-      ("leo",23),
-      ("jack",25)
+    val studentsWithNameAge = Array(
+      ("leo", 23),
+      ("jack", 25)
     ).toSeq
-    val studentWithNameAgeDF = sc.parallelize(studentWithNameAge, 2).toDF("name", "age")
-    studentWithNameAgeDF.write
+    val studentsWtihNameAgeDF = sc.parallelize(studentsWithNameAge, 2).toDF("name","age")
+    studentsWtihNameAgeDF.write
       .mode("overwrite")
       .format("parquet")
       .save("C:\\idea\\spark_demo\\src\\main\\scala\\com\\robin\\file\\students")
 
-    //创建第二个DataFrame,作为学生的成绩信息，并写入一个parquet文件中
-    val studentWithNameGrade = Array(
+    // 创建第二个DataFrame，作为学生的成绩信息，并写入一个parquet文件中
+    val studentsWithNameGrade = Array(
       ("marry","A"),
       ("tom","B")
     ).toSeq
+    val studentsWithNameGradeDF = sc.parallelize(studentsWithNameGrade, 2)toDF("name","grade")
+    studentsWithNameGradeDF.write
+      .mode("append")
+      .format("parquet")
+      .save("C:\\idea\\spark_demo\\src\\main\\scala\\com\\robin\\file\\students")
+
+    val students = sqlContext.read.option("mergeSchema","true")
+      .parquet("C:\\idea\\spark_demo\\src\\main\\scala\\com\\robin\\file\\students")
+
+    students.printSchema()
+    students.show()
 
   }
 }
